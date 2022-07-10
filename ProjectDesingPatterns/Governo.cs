5a3459db89acf863f8ba5e7d5b6fb88358d32cbc
rafaela <rafaela@LAPTOP-P87PD8I2>
2022-07-10 12:00:13 -0300

    Add project files.

---
+++ ProjectDesingPatterns/Governo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDesingPatterns
+{
+    public class Governo
+    {
+
+        private static Governo eleicao = null;
+        private Governo() { }
+
+        public int i = 1;
+        public static Governo Eleicao
+        {
+            get
+            {
+                if (eleicao == null)
+                    eleicao = new Governo();
+                return eleicao;
+            }
+        }
+
+        public void DoSomething()
+        {
+            var a = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location); 
+            string[] lines = File.ReadAllLines(a + "\\ResultadoVotacao.txt");
+        }
+
+    }
+}
---
+++ ProjectDesingPatterns/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectDesingPatterns
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Governo mySingleton2 = Governo.Eleicao;
+            mySingleton2.DoSomething();
+        }
+    }
+
+}
+
