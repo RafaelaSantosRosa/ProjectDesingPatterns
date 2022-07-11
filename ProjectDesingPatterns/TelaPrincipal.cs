39e35766b3f47b8e570b337ece0b883df2287a86
matheusdefigueiredo@gmail.com <matheusdefigueiredo@gmail.com>
2022-07-11 10:58:50 -0300

    Ajustes de tela aplicados.

--- ProjectDesingPatterns/TelaPrincipal.cs
+++ ProjectDesingPatterns/TelaPrincipal.cs
@@ -17,25 +17,38 @@ namespace ProjectDesingPatterns.Telas
 
                 Console.WriteLine("Votar - 1");
                 Console.WriteLine("Ver resultado - 2");
+                Console.WriteLine("Sair - 0");
 
                 opcao = Console.ReadLine();
-
-                if (opcao == "1")
+                switch (opcao)
                 {
+                    case "1":
+                        Cidadao cidadao = new Cidadao();
+                        Console.WriteLine("\nRegistre seu nome: ");
+                        cidadao.nome = Console.ReadLine();
+                        Console.WriteLine("\nEscolha o candidato: \nJosé - 1 \nMaria - 2");
+                        var candidatoEscolhido = Console.ReadLine();
+                        cidadao.candidato = candidatoEscolhido == "1" ? "Jose" : candidatoEscolhido == "2" ? "Maria" : "Erro";
+                        if(cidadao.candidato == "Erro")
+                        {
+                            Console.WriteLine("Erro! Candidato inválido.");
+                            break;
+                        }
+                        Fachada fachada = new Fachada();
+                        fachada.registrarVoto(cidadao);
+                        break;
 
-                    Cidadao cidadao = new Cidadao();
-                    Console.WriteLine("Registre seu nome: ");
-                    cidadao.nome = Console.ReadLine();
-                    Console.WriteLine("Escolha o candidato: \nJosé - 1 \nMaria - 2");
-                    cidadao.candidato = Console.ReadLine() == "1" ? "Jose" : "Maria";
+                    case "2":
+                        Console.Clear();
+                        Console.WriteLine($"\n{Governo.Eleicao.ResultadoEleicao()}\nPressione qualquer tecla para encerrar.");
+                        Console.ReadLine();
+                        opcao = "0";
+                        break;
 
-                    Fachada fachada = new Fachada();
-                    fachada.registrarVoto(cidadao);
-                }
-                else if (opcao == "2")
-                {
-                    Console.Clear();
-                    Console.WriteLine("\n" + Governo.Eleicao.ResultadoEleicao());
+                    case "0": break;
+
+                    default: Console.WriteLine("\nOpção inválida!\n");
+                        break;
                 }
             } while (opcao != "0");
         }
